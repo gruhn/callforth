c7a9271771427a2def7082b0c2d858cdc219396e
Felix Penetro <niklas@gruhn.me>
2020-10-08 15:30:12 +0200

    style: arrow functions / no semi-colons

--- src/callforth.js
+++ src/callforth.js
@@ -1,25 +1,25 @@
 
-export function eventOn(eventTarget, successEvent, errorEvent = "error") {
-  let $resolve, $reject;
+export const eventOn = (eventTarget, successEvent, errorEvent = "error") => {
+  let $resolve, $reject
 
   const promise = new Promise((resolve, reject) => {
-    $resolve = resolve;
-    $reject = reject;
-  });
+    $resolve = resolve
+    $reject = reject
+  })
 
-  eventTarget.addEventListener(successEvent, $resolve);
-  eventTarget.addEventListener(errorEvent, $reject);
+  eventTarget.addEventListener(successEvent, $resolve)
+  eventTarget.addEventListener(errorEvent, $reject)
 
   promise.finally(() => {
-    eventTarget.removeEventListener(successEvent, $resolve);
-    eventTarget.removeEventListener(errorEvent, $reject);
-  });
+    eventTarget.removeEventListener(successEvent, $resolve)
+    eventTarget.removeEventListener(errorEvent, $reject)
+  })
 
-  return promise;
+  return promise
 }
 
-export function timeout(delay) {
+export const timeout = milliseconds => {
   return new Promise(
-    resolve => setTimeout(resolve, delay)
-  );
+    resolve => setTimeout(resolve, milliseconds)
+  )
 }
